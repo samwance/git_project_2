dc6ff6bc25ada4be37b46ebac84a1440e453d04b
semyon <odintsovvans@gmail.com>
2023-06-07 16:08:11 +0600

    imported subtract func

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from funcs import sum_numbers
+from funcs import sum_numbers, subtract_numbers
 
 result = sum_numbers(2, 3)
 print(result)
+
+second_result = subtract_numbers(3, 2)
+print(second_result)
